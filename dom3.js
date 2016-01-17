db41b17ec9721221344b63321e601587c33b2407
Igor <igor.filipiuk@gmail.com>
2016-01-17 19:59:39 +0100

    Create dom3.js

---
+++ dom3.js
@@ -0,0 +1,8 @@
+
+var li = document.createElement("li");
+var bt = document.createElement("button");
+var ul = document.getElementsByTagName('ul')[0];
+
+li.innerText=bt;
+bt.innerText= 'Developer';
+ul.appendChild(bt);
