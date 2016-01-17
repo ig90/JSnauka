047a1d2c9b00553fc3c151500fcad25457b83d8e
Igor <igor.filipiuk@gmail.com>
2016-01-17 17:20:40 +0100

    Update dom2.js

--- dom2.js
+++ dom2.js
@@ -1,7 +1,9 @@
 
 function log(text) {
 
-	document.getElementsByTagName("textarea")[0].innerText = text;
+	//document.getElementsByTagName("textarea")[0].innerText = text;
+	
+	document.getElementsByTagName("textarea")[0].value += text + "\n";
 
 
 }
